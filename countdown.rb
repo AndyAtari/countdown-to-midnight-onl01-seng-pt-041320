4a82097977aeab9f493817a20ff30fb1439b09bd
Andrew Alteri <andrewsalteri@gmail.com>
2020-04-16 12:33:06 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
-#write your code here
 
-def countdown
+
+def countdown(n)
+    n = 11
+    while n > 1
+        n -= 1
+        puts "#{n} SECOND(S)!"
+    end
+        "HAPPY NEW YEAR!"
+end 
+
+
+def countdown_with_sleep(n)
+    while n > 0
+        n -= 1
+    sleep(1)
 end
+end
+
